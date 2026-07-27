df64b57883473f81c1ec285b3cef0f139de27869
Gustavo Siancas <gustavosiancas17@gmail.com>
2026-07-27 09:13:34 -0500

    update

--- config/cloudinaryConfig.py
+++ config/cloudinaryConfig.py
@@ -2,8 +2,8 @@ import cloudinary
 import os
 
 cloudinary.config(
-    cloud_name="dvmj02saf",
-    api_key="234766644624776",
-    api_secret="h5hnetnjAgsGK0aaosH5_Vf8q5s",
+    cloud_name="dvmjsd02saf",
+    api_key="23476664sas4624776",
+    api_secret="h5hne22tnjAgsGK0aaosH5_Vf8q5s",
     secure=True
 )
